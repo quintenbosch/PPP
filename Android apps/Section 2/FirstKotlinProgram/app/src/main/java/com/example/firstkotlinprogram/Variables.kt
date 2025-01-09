6e86454d8f223c4d8ce83d054ca46020952a38a7
quintenbosch <r0785961@student.thomasmore.be>
2025-01-09 17:28:09 +0100

    Section 3 variables

---
+++ Android apps/Section 2/FirstKotlinProgram/app/src/main/java/com/example/firstkotlinprogram/Variables.kt
@@ -0,0 +1,33 @@
+package com.example.firstkotlinprogram
+
+fun main(args: Array<String>) {
+
+    /*
+    var a = 10
+    var b = 20
+    var c = a + b
+
+    println(a + b)
+
+    var age = 10 //valid
+
+    //This is a single comment
+    /* this
+    is
+    multiple comment line
+     */
+
+    var _age = 20 //valid
+    var age3 = 30 //valid
+
+    var a ge = 10 //invalid
+    var 3age = 30 //invalid
+     */
+
+    var age = 20
+    age = 10
+    println(age)
+
+    val year = 2020
+    // year = 2021 //invalid
+}
