f3daeb0bd89e7b190451fca3dc860f60a2f1371f
quintenbosch <r0785961@student.thomasmore.be>
2025-01-10 11:22:21 +0100

    Section 3 Set collection

---
+++ Android apps/Section 2/FirstKotlinProgram/app/src/main/java/com/example/firstkotlinprogram/CollectionsSet.kt
@@ -0,0 +1,15 @@
+package com.example.firstkotlinprogram
+
+fun main(args: Array<String>) {
+    /*
+    var mySetArray = arrayListOf<Any>("Kotlin", 2, 2.5, 'K', true, "Kotlin")
+
+    println(mySetArray.size)
+    println(mySetArray.last())
+     */
+
+    var mySetArray = setOf<Any>("Kotlin", 2, 2.5, 'K', true, "Kotlin")
+
+    println(mySetArray.size)
+    println(mySetArray.last())
+}
