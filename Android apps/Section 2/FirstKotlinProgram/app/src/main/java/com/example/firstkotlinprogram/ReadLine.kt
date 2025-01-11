db8f1a6933c6219abd5ad8b51c6c7e3ad0a28a63
quintenbosch <r0785961@student.thomasmore.be>
2025-01-11 16:59:34 +0100

    Section 4 Console input

---
+++ Android apps/Section 2/FirstKotlinProgram/app/src/main/java/com/example/firstkotlinprogram/ReadLine.kt
@@ -0,0 +1,11 @@
+package com.example.firstkotlinprogram
+
+fun main(args: Array<String>) {
+    print("What is your name? ")
+    var name:String? = readLine()
+
+    print("How old are you? ")
+    var age:Int = readLine()!!.toInt()
+
+    println("Your name is ${name} and your age is " + age)
+}
