c21ee3648dc21cba2f8f32ec26ea4ca0e452de2f
quintenbosch <r0785961@student.thomasmore.be>
2025-01-11 16:34:59 +0100

    Section 4 Equality operators

---
+++ Android apps/Section 2/FirstKotlinProgram/app/src/main/java/com/example/firstkotlinprogram/EqualityOperators.kt
@@ -0,0 +1,13 @@
+package com.example.firstkotlinprogram
+
+fun main(args: Array<String>) {
+    var a:Int = 5
+    var b:Int = 5
+
+    println("a == b : " + (a == b))
+    println("a != b : " + (a != b))
+    println("a > b : " + (a > b))
+    println("a >= b : " + (a >= b))
+    println("a < b : " + (a < b))
+    println("a <= b : " + (a <= b))
+}
