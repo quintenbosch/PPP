59c07f7fc0fa7d75bd0e3c81189b16640f817798
quintenbosch <r0785961@student.thomasmore.be>
2025-01-11 16:51:56 +0100

    Section 4 Range and in

---
+++ Android apps/Section 2/FirstKotlinProgram/app/src/main/java/com/example/firstkotlinprogram/Range.kt
@@ -0,0 +1,8 @@
+package com.example.firstkotlinprogram
+
+fun main(args: Array<String>) {
+    var myCharRange = 'a'.rangeTo('k')
+    var myChar = 'k' in myCharRange
+
+    println("myCharRange has k: " + myChar)
+}
