7ad35465f9e639ae6a0f985929d054a4ab5ab9c4
quintenbosch <r0785961@student.thomasmore.be>
2025-01-11 16:39:39 +0100

    Section 4 Conditional operators

---
+++ Android apps/Section 2/FirstKotlinProgram/app/src/main/java/com/example/firstkotlinprogram/ConditionalOperators.kt
@@ -0,0 +1,19 @@
+package com.example.firstkotlinprogram
+
+fun main(args: Array<String>) {
+    var number1:Int = 5
+    var number2:Int = 8
+    var number3:Int = 12
+
+    var result:Boolean = false
+
+    //Conditionale Operator (&&)
+
+    result = (number1 > number2) && (number3 > number2)
+    println("Result: " + result)
+
+    //Conditionale Operator (||)
+
+    result = (number1 > number2) || (number3 > number2)
+    println("Result: " + result)
+}
