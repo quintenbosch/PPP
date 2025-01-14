987861c013a52791ae2b9c3c76895430ed421ff1
quintenbosch <r0785961@student.thomasmore.be>
2025-01-14 19:05:12 +0100

    Section 8 Oop constructor part 1

---
+++ Android apps/Section 8/ObjectOrientedProgramming/app/src/main/java/com/example/objectorientedprogramming/Cars.kt
@@ -0,0 +1,6 @@
+package com.example.objectorientedprogramming
+
+class Cars {
+    var name:String? = null
+    var model:Int? = null
+}
---
+++ Android apps/Section 8/ObjectOrientedProgramming/app/src/main/java/com/example/objectorientedprogramming/ObjectClassConstructor.kt
@@ -0,0 +1,15 @@
+package com.example.objectorientedprogramming
+
+fun main(args: Array<String>) {
+    var myCar = Cars()
+    myCar.name = "Ferrari"
+    myCar.model = 2021
+
+    var myCar2 = Cars()
+    myCar2.name = "Mercedes"
+    myCar2.model = 2010
+
+    println("My car's name is ${myCar.name} and its model is ${myCar.model}")
+    println("My car's name is ${myCar2.name} and its model is ${myCar2.model}")
+
+}
