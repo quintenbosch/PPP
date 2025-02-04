dc40cde710d8d4331952303309a41121ea17c4ad
quintenbosch <r0785961@student.thomasmore.be>
2025-02-04 13:52:45 +0100

    Section 12 Arrangements & Alignments

--- Android apps/Section 12/Layouts/app/src/main/java/com/example/layouts/MainActivity.kt
+++ Android apps/Section 12/Layouts/app/src/main/java/com/example/layouts/MainActivity.kt
@@ -5,42 +5,91 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.layouts.ui.theme.LayoutsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             LayoutsTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                //MyLayouts(name = "Android")
+                MyAlignments()
             }
         }
     }
 }
 
 @Composable
+fun MyAlignments() {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally)
+        //contentAlignment = Alignment.Center) for Box layout
+    {
+        Text(text = "Hello",
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.background(Color.Red).padding(start = 20.dp, end = 30.dp))
+
+        Spacer(modifier = Modifier.size(20.dp))
+
+        Text(text = "Android",
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.background(Color.Red).width(100.dp))
+
+        Spacer(modifier = Modifier.size(20.dp))
+
+        Text(text = "Kotlin",
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.background(Color.Red).width(100.dp))
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun MyLayouts(name: String) {
     Column {
         Box(modifier = Modifier.background(Color.Red).height(200.dp).width(300.dp)) {
             Row {
@@ -52,16 +101,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             }
         }
     }
-
-
-
-
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LayoutsTheme {
-        Greeting("Android")
+        //Greeting("Android")
+        //MyLayouts("Android")
+        MyAlignments()
     }
 }
