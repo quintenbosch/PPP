978a8882199253d9842519550491f986c2cd7aa9
quintenbosch <r0785961@student.thomasmore.be>
2025-02-08 11:23:57 +0100

    Section 16 Project 1 part 2

--- Android apps/Section 16/TODOList/app/src/main/java/com/example/todolist/MainActivity.kt
+++ Android apps/Section 16/TODOList/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,16 +15,25 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -32,6 +42,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.todolist.ui.theme.TODOListTheme
@@ -56,6 +67,11 @@ fun MainPage() {
     val todoName = remember {
         mutableStateOf("")
     }
+
+    val itemList = remember {
+        mutableStateListOf("Learn Kotlin", "Learn Compose")
+    }
+
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -100,6 +116,49 @@ fun MainPage() {
                 ) {
                 Text(text = "Enter TODO", fontSize = 20.sp)
             }
+        }
+        LazyColumn {
+            items(
+                count = itemList.size,
+                itemContent = { index ->
+                    val item = itemList[index]
+
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 1.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            contentColor = Color.White
+                        ),
+                        shape = RoundedCornerShape(0.dp)
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth().padding(10.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = item,
+                                color = Color.White,
+                                fontSize = 18.sp, maxLines = 2,
+                                overflow = TextOverflow.Ellipsis,
+                                modifier = Modifier.width(300.dp)
+                            )
+
+                            Row {
+                                IconButton(onClick = {}) {
+                                    Icon(Icons.Filled.Edit, contentDescription = "Edit", tint = Color.White)
+                                }
+                                IconButton(onClick = {}) {
+                                    Icon(Icons.Filled.Delete, contentDescription = "Delete", tint = Color.White)
+                                }
+                            }
+
+                        }
+                    }
+                }
+            )
         }
     }
 }
